18231a63f224915896e98fbfbeac0fa7b3c4f19a
EnginS <you@example.com>
2024-11-30 18:53:47 +0300

    logo yer değişikliği

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ import "/my-bulma.scss"
 
 document.querySelector('#app').innerHTML = `
 <div>
-<a href="https://vite.dev" target="_blank"><img src="${viteLogo}" class="logo" alt="Vite logo" /></a>
 <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" /></a>
+<a href="https://vite.dev" target="_blank"><img src="${viteLogo}" class="logo" alt="Vite logo" /></a>
 <button class="button" id="counter" type="button"></button>
 </div>
 `
