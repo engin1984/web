8b4a184bf98f2d9849bac0434bf9a97f4bbd1afc
EnginS <you@example.com>
2024-12-01 15:35:59 +0300

    remove counter

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // ########## vite
-import javascriptLogo from './img/javascript.svg'
-import viteLogo from '/img/vite.svg'
-import { setupCounter } from './counter.js'
+// import javascriptLogo from './img/javascript.svg'
+// import viteLogo from '/img/vite.svg'
+// import { setupCounter } from './counter.js'
+
+
+
+// document.querySelector('#app').innerHTML = `
+// <div>
+// <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" /></a>
+// <a href="https://vite.dev" target="_blank"><img src="${viteLogo}" class="logo" alt="Vite logo" /></a>
+// <button class="button" id="counter" type="button"></button>
+// </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+
 import "/my-bulma.scss"
-
-
-document.querySelector('#app').innerHTML = `
-<div>
-<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" /></a>
-<a href="https://vite.dev" target="_blank"><img src="${viteLogo}" class="logo" alt="Vite logo" /></a>
-<button class="button" id="counter" type="button"></button>
-</div>
-`
-
-setupCounter(document.querySelector('#counter'))
-
-
 
 // ########## bulma menu burger
   document.addEventListener('DOMContentLoaded', () => {
@@ -95,11 +95,48 @@ setupCounter(document.querySelector('#counter'))
 
 import { Chessground } from './node_modules/chessground/dist/chessground.js';
 
-Chessground(document.getElementById('board-1'), {});
+Chessground(document.getElementById('board-1'), {
+
+  
+    orientation: "white",   // board orientation (or view angle) "white" | "black"
+    turnColor: "white",     // turn to play. "white" | "black"
+    check: null,            // square currently in check "a2" | null
+    lastMove: null,         // squares part of the last move ["c3", "c4"] | null
+    selected: null,         // square currently selected "a1" | null
+    coordinates: true,      // display board coordinates as square ::after elements
+    viewOnly: false,        // don't bind events: the user will never be able to move pieces around
+    minimalDom: false,      // don't use square elements. Optimization to use only with viewOnly
+    disableContextMenu: false,
+
+    highlight: {
+      lastMove: true,       // add last-move class to squares
+      check: true,          // add check class to squares
+      dragOver: true        // add drag-over class to square when dragging over it
+    },
+
+    draggable: {
+      enabled: true,        // allow moves & premoves to use drag'n drop
+      distance: 3,          // minimum distance to initiate a drag, in pixels
+      squareTarget: false,  // display big square target; intended for mobile
+      centerPiece: true,    // center the piece on cursor at drag start
+      showGhost: true,      // show ghost of piece being dragged
+    },
+    
+  movable: {
+    free: false
+},
+drawable: {
+  enabled: true         // enable SVG circle and arrow drawing on the board
+},
+
+});
 Chessground(document.getElementById('board-2'), {
   fen: 'r2q2k1/1p6/p2p4/2pN1rp1/N1Pb2Q1/8/PP1B4/R6K b - - 2 25',
 });
 Chessground(document.getElementById('board-3'), {
+  
+ 
+  
   drawable: {
     autoShapes: [
       {
